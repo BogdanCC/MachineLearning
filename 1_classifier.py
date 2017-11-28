2eebe3535e38ef09eedcc691e46f01ba16db52dd
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2017-11-28 10:44:40 +0200

    Another Classifier

--- 1_classifier.py
+++ 1_classifier.py
@@ -1,5 +1,5 @@
 from sklearn import tree
-
+from sklearn.naive_bayes import GaussianNB
 # Supervised Learning - we give examples(features and correct label) to a program to learn from. Then we can give it data (features) to make predictions for us based on what it learned
     # 1.Collect training data - 2. Train Classifier - 3. Make predictions
 
@@ -23,8 +23,10 @@ clf = clf.fit(features, labels)
 # Step 3 : Now that we have trained our classifier, we can give it inputs to make predictions for us(to label them)
     # we give inputs by calling the predit() function and passing as arguments new features (so an array with 2 bits of data in our case)
     # the classifier will label each set of features based on what it learned
-print(clf.predict([[160, 0], [80, 0], [144, 1]]))
-
+test_data = [[160, 0], [130, 0], [144, 1]]
+test_labels = [1, 1, 0]
+print("\nCorrect results : \n", *test_labels)
+print("Program predicts(Tree clf) : \n", *clf.predict(test_data), "\n\n---------------------\n")
 # Creating my own classifier - it should predict if a user liked a video or not based on 5 features we'll give it
 # Based on these features, the label will simply be either liked or not liked (represented by 1 and 0 accordingly)
 """
@@ -41,11 +43,18 @@ print(clf.predict([[160, 0], [80, 0], [144, 1]]))
         0 - not liked
 """
 # Create data - features and their correct labels
-features2 = [[30, 0, 1, 2, 0], [28, 0, 0, 2, 0], [68, 0, 1, 1, 0], [92, 1, 1, 1, 1], [67, 1, 1, 2, 0], [24, 0, 1, 0, 0], [94, 0, 0, 1, 1]]
-lables2 = [0, 0, 0, 1, 1, 0, 1]
+features2 = [[30, 0, 1, 2, 0], [28, 0, 0, 2, 0], [68, 0, 1, 1, 0], [92, 1, 1, 1, 1],
+             [67, 1, 1, 2, 0], [24, 0, 1, 0, 0], [94, 0, 0, 1, 1], [53, 0, 0, 0, 0]]
+labels2 = [0, 0, 0, 1, 1, 0, 1, 0]
 # Train the classifier with our dataw
-clf2 = tree.DecisionTreeClassifier().fit(features2, lables2)
+clfG = GaussianNB().fit(features2, labels2)
+clfT = tree.DecisionTreeClassifier().fit(features2, labels2)
 # Label the features we're passing in
-clf2 = clf2.predict([[22,0,1,2,0], [82,1,1,1,1], [95,1,0,1,1], [32,0,0,0,0], [50, 1, 0, 0, 0], [50, 0, 0, 0, 0]])
-# Print results
-print("Results are : ", clf2)
+test_data = [[22,0,1,2,0], [82,1,1,1,1], [95,1,0,1,1], [32,0,0,0,0], [50, 1, 0, 0, 0], [50, 0, 0, 0, 0]]
+test_label = [0,1,1,0,1,0]
+clfG = clfG.predict(test_data)
+clfT = clfT.predict(test_data);
+# Print Results
+print("My correct results : \n", *test_label)
+print("(Dec. Tree) Predicted Results are : \n", *clfT)
+print("(Naive Bayes) Predicted Results are : \n", *clfG)
---
+++ 2_another_classifier.py
@@ -0,0 +1,48 @@
+# Steps for this program
+    # 1. Import data set
+    # 2. Train classifier
+    # 3. Predict label for new data
+
+# Import the iris data set from sklean ( this is a table with 150 examples of 3 types of flower - 50 examples each)
+# The features are : Sepal length, Sepal width, Petal length, Petal width
+# The labels are (the flower species) : Setosa, Versicolor, Virginica
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
+iris_data = load_iris()
+# target_names returns the metadata for the labels : Setosa, Versicolor, Virginica
+print("Label names: ", iris_data.target_names)
+# feature_names returns the metadata for the features : Sepal length, Sepal width, Petal length, Petal width
+print("Feature names : ", iris_data.feature_names)
+# data[] returns the features data
+print(iris_data.data[0])
+# target[] returns the label data
+print(iris_data.target[0])
+# using the for loop to show all data
+for i in range(len(iris_data.data)):
+    print("Label : {}; Features : {}".format(iris_data.target[i], iris_data.data[i]))
+
+# Now we can train the classifier but first we need to remove at least one flower example so we can feed it in afterwards and see if it guesses correctly
+# Aka training data vs testing data
+# We will remove first setos, first versicolor, first virginica
+import numpy as np
+data_to_remove = [[0, 1, 50, 10, 51, 3, 100, 121]]
+
+# training data (axis 0 simply means we're deleting rows, not columns)
+training_data = np.delete(iris_data.data, data_to_remove, axis = 0)
+training_target = np.delete(iris_data.target, data_to_remove)
+
+# testing data
+test_data = iris_data.data[data_to_remove]
+test_target = iris_data.target[data_to_remove]
+
+# 2. Training classifier - Let's use 2 different learning algorithms
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(training_data, training_target)
+clfG = GaussianNB()
+clfG.fit(training_data, training_target)
+
+# 3. Making predictions
+print("Correct labels for test data :\n", test_target)
+print("(Dec. Tree) Program predicts :\n", clf.predict(test_data))
+print("(Naive Bayes) Program predicts : \n", clfG.predict(test_data))
