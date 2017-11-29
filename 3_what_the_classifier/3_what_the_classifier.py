532dc9fc0f62b698d53220436cd7436e808ae376
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2017-11-29 12:54:41 +0200

    Update 3_what_the_classifier.py

--- 3_what_the_classifier/3_what_the_classifier.py
+++ 3_what_the_classifier/3_what_the_classifier.py
@@ -1,16 +1,16 @@
 # In this program we will use 3 different classifiers and check their accuracy
-# The purpose of today is to think of the classifiers as a function
+# The purpose of today is to think of the algorithm used by the model(classifier) as a function
 
 # Import and load toy iris data set
 from sklearn import datasets as ds
 iris_data = ds.load_iris()
 
-# Now we will name the features and labels with X and y in order to think of this as a function
+# Now we will name the features and labels X and y accordingly, in order to think of this as a function
 # f(x) = y ; where x = features and y = labels
 X = iris_data.data # features
 y = iris_data.target # labels
 
-# Now we can split the data from the iris dataset, so we have our training data and our testing data
+# Now we can split the data from the iris dataset in half, so we have our training data and our testing data
 # A little handy utility from sklearn
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5) # this will split the data in 2 (50% test data, 50% train data)
@@ -25,12 +25,12 @@ clfT = tree.DecisionTreeClassifier()
 clfG = GaussianNB()
 clfN = KNeighborsClassifier()
 
-# Train classifiers with train data
+# Train classifiers with the training data
 clfT.fit(X_train, y_train)
 clfG.fit(X_train, y_train)
 clfN.fit(X_train, y_train)
 
-# Make predictions from test data
+# Predict the label for these features (testing data)
 predictionT = clfT.predict(X_test)
 predictionG = clfG.predict(X_test)
 predictionN = clfN.predict(X_test)
